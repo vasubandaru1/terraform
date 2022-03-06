26fd3995182991a89c82144c09bc9b0ba04b302b
vasu <bandaruvasu1@gmail.com>
2022-03-06 13:34:33 +0530

    ip

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
   count         = var.env == "prod" ? 1 : 0
-  ami           =  "data.aws_ami.example.id"
+  ami           =  data.aws_ami.example.id
   instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
   vpc_security_group_ids = [var.SGID]
 
@@ -17,7 +17,7 @@ variable "env" {}
 
 data "aws_ami" "example" {
   most_recent      = true
-  name_regex       = "ubuntu"
+  name_regex       = "^centos*"
   owners           = ["973714476881"]
 
 }
