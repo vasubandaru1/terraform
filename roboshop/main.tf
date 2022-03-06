b3096ec3a7d4cb3982f9ed799153c1667acb3e61
vasu <bandaruvasu1@gmail.com>
2022-03-06 22:04:12 +0530

    ip

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,22 +1,22 @@
 resource "aws_spot_instance_request" "cheap_worker" {
-  count         = length(var.component)
+  count         = length(var.components)
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0fcfa1dc4218b8b12"]
 
   tags = {
-    Name = element(var.component, count.index )
+    Name = element(var.components, count.index)
 
   }
 }
 
 data "aws_ami" "example" {
   most_recent = true
-  name_regex  = "^Cent*"
+  name_regex  = "^Centos"
   owners      = ["973714476881"]
 }
 
-variable "component" {
+variable "components" {
   default = [ "frontend", "mongodb", "catalogue", "cart", "user", "redis", "mysql", "shipping", "rabbitmq", "payment" ]
 }
 
