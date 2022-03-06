a72882920815e0998e970af4674b8c5eb426be73
vasu <bandaruvasu1@gmail.com>
2022-03-06 13:59:03 +0530

    ip

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -15,9 +15,10 @@ variable "name" {}
 variable "instance_type" {}
 variable "env" {}
 
-data "aws_ami" "example"  {
-  most_recent = true
-  name_regex  = "^centos*"
-  owners      = ["973714476881"]
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "^centos*"
+  owners           = ["973714476881"]
+
 
 }
