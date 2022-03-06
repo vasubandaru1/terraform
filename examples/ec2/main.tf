48eef04190fce37ec6842aaf4495a381b8a9c0a5
vasu <bandaruvasu1@gmail.com>
2022-03-06 20:54:52 +0530

    ip

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -1,13 +1,31 @@
 resource "aws_instance" "sample" {
-  count         = var.env == "prod" ? 1 : 0
-  ami           = data.aws_ami.example.id
-  instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
+  count                  = var.env == "prod" ? 1 : 0
+  ami                    = data.aws_ami.example.id
+  instance_type          = var.instance_type == "" ? "t3.micro" : var.instance_type
   vpc_security_group_ids = [var.SGID]
 
   tags = {
     Name = element(var.name, count.index)
   }
 }
+resource "null_resource" "sample" {
+  triggers = {
+    abc = aws_instance.sample.*.private_ip[0]
+  }
+  provisioner "remote-exec" {
+    connection {
+      host = "self.public_ip"
+      user = "Centos"
+      password = "DevOps321"
+    }
+    inline = [
+            "echo hello"
+    ]
+  }
+
+
+}
+
 
 
 variable "SGID" {}
