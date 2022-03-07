63a92f9316e9aeae233170917ce8f5075d247632
vasu <bandaruvasu1@gmail.com>
2022-03-07 13:50:41 +0530

    ip

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -6,16 +6,32 @@ resource "aws_spot_instance_request" "cheap_worker" {
   wait_for_fulfillment = true
 
   tags = {
-    Name = element(var.components, count.index)
+    Name = local.COMP_NAME
 
   }
 }
+
+resource "null_resource" "ansible" {
+  count  = length(var.components)
+  provisioner "remote-exec" {
+    host        = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+    user        = Centos
+    password    = DevOps321
+  }
+    inline = [
+      "yum install python3-pip -y",
+      "pip3 install pip --upgrade",
+      "pip3 install ansible",
+      "ansible-pull -U https://github.com/vasubandaru1/ANSIBLE2.git roboshop-pull.yml -e ENV=dev -e COMPONENT=${local.COMP_NAME}"
+    ]
+}
+
 
 resource "aws_ec2_tag" "tags" {
   count       = length(var.components)
   key         = "Name"
   resource_id = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-  value       = element(var.components, count.index)
+  value       = local.COMP_NAME
 }
 
 data "aws_ami" "ami" {
@@ -30,4 +46,8 @@ variable "components" {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+locals {
+  COMP_NAME = element(var.components, count.index)
 }
