5c7ed585724f9235f47a49710458a5a3e13cd7e2
vasu <bandaruvasu1@gmail.com>
2022-03-08 12:20:50 +0530

    ip

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "cheap_worker" {
   count                  = length(var.components)
   ami                    = data.aws_ami.ami.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0fcfa1dc4218b8b12"]
   wait_for_fulfillment = true
 
